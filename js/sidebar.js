4f4edb609351dc95e1f2413c6e36b56af41fedf0
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2017-01-04 16:35:48 +1100

    populate sidebar from all books

---
+++ js/sidebar.js
@@ -0,0 +1,37 @@
+function populateSidebar(data)
+{
+ d3.select("#booklist")
+   .append("ul")
+   .attr("id", "books")
+   .selectAll("li")
+   .data(data)
+   .enter().filter(function(d){return d.name.length > 1})
+   .append("li")
+   .text(function(d) {return d.name;})
+   .style("color", function(d){
+   	  var fillcol = d.read=="0"? "red" : "#66cc00";
+  	  return fillcol;
+   })
+}
+
+function organiseByGenre(event)
+{
+  var svg = d3.select("#bookviz").append("svg")
+	  .attr("id","bookvizsvg")
+	  .attr("width", 800)
+	  .attr("height", 960);
+
+  var nested_data = d3.nest()
+				.key(function(d){return d.genre})
+				.entries(event.data);
+
+}
+
+$(document).ready(function () {
+	d3.csv("/mybookshelf/data/allbooks.csv", function(error, data){	
+   		populateSidebar(data)
+    	$( "#bygenre" ).click(data,organiseByGenre);
+	});	
+});
+
+
